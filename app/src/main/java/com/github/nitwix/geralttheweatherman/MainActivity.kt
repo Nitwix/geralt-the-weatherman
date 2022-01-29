50d95c7db0f64d60024cbe1ddb4ab3c708abeac9
Niels Lachat <niels.lachat@epfl.ch>
2022-01-29 17:13:48 +0100

    Main Activity designed
    - TextView for weather commentary
    - Refresh button in action bar for refreshing

--- app/src/main/java/com/github/nitwix/geralttheweatherman/MainActivity.kt
+++ app/src/main/java/com/github/nitwix/geralttheweatherman/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.github.nitwix.geralttheweatherman
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+typealias ResId = Int
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    fun onRefreshClick(item: MenuItem) {
+        val weatherCommentaries: List<String> =
+            resources.getStringArray(R.array.weather_commentaries).toList()
+        val tv = findViewById<TextView>(R.id.activity_main_geralt_wc_text_view)
+        tv.text = weatherCommentaries.random()
+    }
 }
+
